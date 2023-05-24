31415e4d368441218acab0068f20c96ff6f66c86
Bahdan Kalishok <bahdan_kalishok@epam.com>
2023-05-24 11:04:25 +0600

    Change return type for Get of categories

--- RestArchitecture/RestArchitecture/Handlers/Categories/GetCategoriesHandler.cs
+++ RestArchitecture/RestArchitecture/Handlers/Categories/GetCategoriesHandler.cs
@@ -2,10 +2,11 @@
 using Infrastructure.Models;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using RestArchitecture.Models;
 
 namespace RestArchitecture.Handlers.Categories
 {
-    public class GetCategoriesHandler : IRequestHandler<GetCategoriesRequest, List<Category>>
+    public class GetCategoriesHandler : IRequestHandler<GetCategoriesRequest, List<CategoryDto>>
     {
         private readonly CatalogContext _dbContext;
 
@@ -14,9 +15,15 @@ namespace RestArchitecture.Handlers.Categories
             _dbContext = dbContext;
         }
 
-        public async Task<List<Category>> Handle(GetCategoriesRequest request, CancellationToken cancellationToken)
+        public async Task<List<CategoryDto>> Handle(GetCategoriesRequest request, CancellationToken cancellationToken)
         {
-            return await _dbContext.Categories.ToListAsync(cancellationToken);
+            return await _dbContext.Categories
+                .Select(x => new CategoryDto
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                })
+                .ToListAsync(cancellationToken);
         }
     }
 }
